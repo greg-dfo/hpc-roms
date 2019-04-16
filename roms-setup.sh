5296b513f93ed350718280fa3c70dee5d7506c42
Greg Cormier <greg.cormier@dfo.mpo.ca>
2019-04-16 16:14:10 -0400

    initial

---
+++ roms-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sudo apt install perl diffutils
+
+sudo apt install libnetcdff6 netcdf-bin libnetcdff-dev
+
+sudo apt install gfortran-4.8 gfortran-4.8-multilib libgfortran4-amd64-cross --install-suggests
+
+sudo apt install mpi-default-bin mpi-default-dev --install-suggests
